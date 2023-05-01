dc2f2f1a0deff9a20410cbd23643df4215a1b61a
Tiago Costa <tiagodpv@gmail.com>
2023-05-01 13:58:20 -0400

    add f

--- index.js
+++ index.js
@@ -15,7 +15,29 @@ app.use(
 app.use(express.json());
 
 //Rotas da API
-app.post('person', (req, res) => {});
+app.post('/person', async (req, res) => {
+  //req.body
+
+  // { name: "Tiago", salary: 50000, approved: false }
+  const { name, salary, aproved } = req.body;
+
+  const person = {
+    name,
+    salary,
+    approved,
+  };
+
+  try {
+    //crinado dados
+    await Person.create(person);
+
+    res
+      .status(201)
+      .json({ message: 'Pessoa inserida no sistema com sucesso!' });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
 
 // rota inicial / endpoint
 app.get('/', (req, res) => {
